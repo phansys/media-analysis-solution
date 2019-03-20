85a1c6df4e2f3bffbac9268a60bed4234eb181bd
Javier Fiore <jfiore83@gmail.com>
2019-03-20 15:10:51 -0300

    Fix adding callback on fs.writeFile() function.

--- deployment/manifest-generator/app.js
+++ deployment/manifest-generator/app.js
@@ -80,5 +80,8 @@ for (let i = 0; i < _filelist.length; i++) {
     _manifest.files.push(_filelist[i].replace(`${args.target}/`, ''));
 };
 
-fs.writeFile(args.output, JSON.stringify(_manifest, null, 4));
+fs.writeFile(args.output, JSON.stringify(_manifest, null, 4), function(err, result) {
+     if(err) console.log('error', err);
+});
+
 console.log(`Manifest file ${args.output} generated.`);
