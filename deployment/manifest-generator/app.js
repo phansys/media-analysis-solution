c82a905d76ba64a6f0a8d0d63e82980a6158f9e6
Javier Fiore <jfiore83@gmail.com>
2019-03-20 15:32:12 -0300

    Change according coding standard.

--- deployment/manifest-generator/app.js
+++ deployment/manifest-generator/app.js
@@ -81,7 +81,9 @@ for (let i = 0; i < _filelist.length; i++) {
 };
 
 fs.writeFile(args.output, JSON.stringify(_manifest, null, 4), function(err, result) {
-     if(err) console.log('error', err);
+    if (err) {
+        console.error(err);
+    }
 });
 
 console.log(`Manifest file ${args.output} generated.`);
