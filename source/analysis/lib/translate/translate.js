bcfa1edce18a59e6444ddf61d1c9b517b9f8a8e2
Javier Spagnoletti <phansys@gmail.com>
2019-03-27 15:05:29 -0300

    Update `translate.js`

--- source/analysis/lib/translate/translate.js
+++ source/analysis/lib/translate/translate.js
@@ -16,12 +16,9 @@
  */
 
 'use strict';
-let PATH = require('path');
-let crypto = require('crypto');
 let AWS = require('aws-sdk');
 let upload = require('../upload');
 let request = require('request');
-let creds = new AWS.EnvironmentCredentials('AWS');
 
 const s3Bucket = process.env.S3_BUCKET;
 
@@ -32,13 +29,13 @@ const s3Bucket = process.env.S3_BUCKET;
  * @class translate
  */
 
- let translate = (function() {
+let translate = (function () {
 
   /**
-  * @class translate
-  * @constructor
-  */
-  let translate = function() {};
+   * @class translate
+   * @constructor
+   */
+  let translate = function () {};
 
   /**
    * Starts text translation job
@@ -46,26 +43,24 @@ const s3Bucket = process.env.S3_BUCKET;
    * @param {startTranslation~callback} cb - The callback that handles the response.
    */
 
-  translate.prototype.startTranslation = function(event_info, cb) {
+  translate.prototype.startTranslation = function (event_info, cb) {
     console.log('Executing text translation');
 
-    let key = event_info.key;
+    let key = event_info.results.transcript.key;
     let language_code = (event_info.ai_options || {}).language_code || 'en-US';
     let source_text = (event_info.ai_options || {}).source_text || '';
     let target_language_code = (event_info.ai_options || {}).target_language_code || 'es-US';
 
     let source_file_uri = '';
-    if (process.env.AWS_REGION == 'us-east-1'){
-        source_file_uri = ['https://s3.amazonaws.com',s3Bucket,key].join('/');
+    if ('us-east-1' === process.env.AWS_REGION) {
+      source_file_uri = ['https://s3.amazonaws.com', s3Bucket, '/', key].join('');
+    } else {
+      source_file_uri = ['https://s3-', process.env.AWS_REGION, '.amazonaws.com', '/', s3Bucket, '/', key].join('');
     }
-    else {
-        source_file_uri = ['https://s3-',process.env.AWS_REGION,'.amazonaws.com/',s3Bucket,'/',key].join('');
-    }
-    console.log('File:: ',source_file_uri, 'language_code:: ', language_code);
+    console.log('File:: ', source_file_uri, 'language_code:: ', language_code);
 
-    // @todo: handle errors on `request()` call
-    source_text = request(source_file_uri, function(err, data, body) {
-        return body;
+    source_text = request(source_file_uri, function (err, data, body) {
+      return body;
     });
 
     let params = {
@@ -75,63 +70,35 @@ const s3Bucket = process.env.S3_BUCKET;
     };
 
     let translate = new AWS.Translate();
-    translate.translateText(params, function(err, data) {
-
-      // 07.02.2018 - Prevents infinite loop when translate job doesn't start
-      // If the translation job returns an error, pass it on to the state machine.
+    translate.translateText(params, function (err, data) {
       if (err) {
-        let resp = {
-          jobDidStart: false,
-          err: err
+        return cb(err, null);
+      } else {
+
+        var translatedText = data.TranslatedText;
+
+        let text_key = ['private', event_info.owner_id, 'media', event_info.object_id, 'results', 'translated_text_' + params.TargetLanguageCode + '.json'].join('/');
+
+        let s3_params = {
+          Bucket: s3Bucket,
+          Key: text_key,
+          Body: JSON.stringify(data),
+          ContentType: 'application/json'
         };
 
-        console.error(resp);
-
-        return cb(resp, null);
-      }
-
-      // 07.02.2018 - Prevents infinite loop when translate job doesn't start
-      // Otherwise, return information about the job to the state machine.
-      else {
-        let resp = {
-          jobDidStart: true,
-          jobName: data.TranslatedText
-        }
-
-        console.log(resp);
-
-        return cb(null, resp);
+        upload.respond(s3_params, function (err, response) {
+          if (err) {
+            return cb(err, null);
+          } else {
+            let text_response = {'key': text_key, 'text': translatedText, 'status': "COMPLETE"};
+            return cb(null, text_response);
+          }
+        });
       }
     });
-
-    // request(data.TranscriptionJob.Transcript.TranscriptFileUri, function(err, data, body) {
-    //   if (err) {
-    //     return Promise.reject(err);
-    //   }
-    //   else {
-    //     let transcript_json = JSON.parse(body);
-    //     let transcript_key = ['private',event_info.owner_id,'media',event_info.object_id,'results','transcript.json'].join('/');
-    //     let s3_params = {
-    //         Bucket: s3Bucket,
-    //         Key: transcript_key,
-    //         Body: JSON.stringify(transcript_json),
-    //         ContentType: 'application/json'
-    //     };
-
-    //     upload.respond(s3_params, function(err, data) {
-    //         if (err){
-    //           return Promise.reject(err);
-    //         }
-    //         else {
-    //           let transcript_response = {'key': transcript_key, 'status': 'COMPLETE'};
-    //           return resolve(transcript_response);
-    //         }
-    //     });
-    //   }
-    // });
   };
 
   return translate;
- })();
+})();
 
- module.exports = translate;
+module.exports = translate;
