cf1a89ab071c74a5b10c37427615ac87e48a861b
Javier Spagnoletti <phansys@gmail.com>
2019-08-21 17:58:38 -0300

    Allow to create SRT files from transcript source

---
+++ source/analysis/lib/srt_create/srt_create.js
@@ -0,0 +1,115 @@
+/*********************************************************************************************************************
+ *  Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.                                           *
+ *                                                                                                                    *
+ *  Licensed under the Amazon Software License (the "License"). You may not use this file except in compliance        *
+ *  with the License. A copy of the License is located at                                                             *
+ *                                                                                                                    *
+ *      http://aws.amazon.com/asl/                                                                                    *
+ *                                                                                                                    *
+ *  or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES *
+ *  OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions    *
+ *  and limitations under the License.                                                                                *
+ *********************************************************************************************************************/
+
+/**
+ * @author Solution Builders
+ */
+
+'use strict';
+let AWS = require('aws-sdk');
+let upload = require('../upload');
+let tsToSrt = require('aws-transcription-to-srt');
+
+const s3Bucket = process.env.S3_BUCKET;
+
+/**
+ * Creates a SRT file from transcribe content
+ *
+ * @class srt_create
+ */
+
+let srt_create = (function () {
+
+  /**
+   * @class srt_create
+   * @constructor
+   */
+  let srt_create = function () { };
+
+  /**
+   * Starts text translation job
+   * @param {JSON} event_info - information about the text file
+   * @param {startTranslation~callback} cb - The callback that handles the response.
+   */
+
+  srt_create.prototype.convert = function (event_info, cb) {
+    console.log('Executing SRT creation');
+
+    let key = event_info.results.transcript.key;
+    let source_text = (event_info.ai_options || {}).source_text || '';
+
+    console.log('Key:: ', key);
+
+    let transcript_params = {
+      Bucket: s3Bucket,
+      Key: key
+    };
+
+    getTranscript(transcript_params, function (err, data) {
+      if (err) {
+        return cb(err, null);
+      }
+
+      const transcript = JSON.parse(data.Body.toString('utf-8'));
+
+      source_text = transcript.results.transcripts[0].transcript;
+
+      convertTsToSrt(source_text)
+        .then((results) => {
+          storageS3(event_info, results, cb);
+        });
+
+    });
+  };
+
+  const storageS3 = function (event_info, srt, cb) {
+    let text_key = ['private', event_info.owner_id, 'media', event_info.object_id, 'results', 'transcript.srt'].join('/');
+
+    let s3_params = {
+      Bucket: s3Bucket,
+      Key: text_key,
+      Body: srt,
+      ContentType: 'text/plain'
+    };
+
+    upload.respond(s3_params, function (err, response) {
+      if (err) {
+        return cb(err, null);
+      }
+
+      const text_response = {'key': text_key, 'status': "COMPLETE"};
+
+      return cb(null, text_response);
+    });
+  }
+
+  const getTranscript = function (params, cb) {
+    let s3 = new AWS.S3();
+    s3.getObject(params, function (err, data) {
+      if (err) {
+        console.log(err);
+        return cb(err, null);
+      } else {
+        return cb(null, data);
+      }
+    });
+  };
+
+  const convertTsToSrt = function (ts) {
+    return tsToSrt(ts);
+  };
+
+  return srt_create;
+})();
+
+module.exports = srt_create;
